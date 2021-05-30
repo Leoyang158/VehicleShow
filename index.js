e8942966b5486a9a64bdfdca1c9d058bb4951997
Linjian Yang <62164871+Leoyang158@users.noreply.github.com>
2021-05-29 23:35:39 -0500

    mongoose set up on localhost
    mongoose!!!

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/vehicleShow', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
 // app.use(methodOverride('_method'));
 ///////////////////////////////
 // var axios = require("axios").default;
