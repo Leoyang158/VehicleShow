a011fdfb8e4a61796f543cee2af03a2017cf48d9
Linjian Yang <62164871+Leoyang158@users.noreply.github.com>
2021-06-01 00:57:24 -0500

    CRUD
    update CRUD with api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Vehicle = require('./models/savedCar');
 const request = require('request');
+const { RSA_NO_PADDING } = require("constants");
 
 mongoose.connect('mongodb://localhost:27017/vehicleShow', { 
     useNewUrlParser: true, 
@@ -25,31 +26,6 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-// var axios = require("axios");
-// const { response } = require("express");
-// var options = {
-//   method: 'GET',
-//   url: 'https://car-data.p.rapidapi.com/cars',
-//   params: {limit: '10', page: '0'},
-//   headers: {
-//     'x-rapidapi-key': '20d6c5a99bmsh5f5da4f8b6aa626p101941jsn1c9e0828d976',
-//     'x-rapidapi-host': 'car-data.p.rapidapi.com'
-//   }
-// };
-const options = {
-  method: 'GET',
-  url: 'https://car-data.p.rapidapi.com/cars',
-  qs: {limit: '10', page: '0'},
-  headers: {
-    'x-rapidapi-key': '20d6c5a99bmsh5f5da4f8b6aa626p101941jsn1c9e0828d976',
-    'x-rapidapi-host': 'car-data.p.rapidapi.com',
-    useQueryString: true
-  }
-};
-//
-//////////////////////////////
-
-
 app.get('/', (req, res) => {
     res.render('welcome')
 });
@@ -66,32 +42,48 @@ app.post('/search', (req, res) => {
     // res.send(username)
 });
 
-app.get('/list', async (req, res) =>{
-    // fetch api
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        const cars = JSON.parse(body);
-        // res.render('list', {cars} );
-        const seedDB = async () => {
-            await Vehicle.deleteMany({});
-            for(let car of cars){
-                const vehicle = new Vehicle({
-                    year: `$year`,
-                    make: make,
-                    model: model,
-                    type:  type
-                });
-                await vehicle.save();
-            }
-        }
-        seedDB();
-    });
+app.get('/lists', async (req, res) =>{
+    const cars = await Vehicle.find({})
+    res.render('lists/list', {cars});
 })
 
-app.get('list.:id', async (req, res) => {
-    const car = await Vehicle.findById(req.params.id)
-    res.render('show', {car});
+app.get('/lists/new', (req,res) => {
+    res.render('lists/new');
 })
+
+app.post('/lists', async (req, res) => {
+    const newCar = new Vehicle(req.body.list);
+    console.log(newCar);
+    // console.log(newCar._id);
+    res.redirect('/lists');
+    // await newCar.save();
+    // res.redirect(`/lists/${newCar._id}`);
+})
+
+//////////////////////////////////////////////////////
+app.get('/lists/:id', async (req, res) => {
+    const car = await Vehicle.findById(req.params.id);
+    res.render('lists/show', {car});
+})
+
+app.get('/lists/:id/edit', async (req, res) => {
+    const car = await Vehicle.findById(req.params.id)
+    res.render('lists/edit', { car });
+})
+
+// app.put('/lists/:id', async (req, res) => {
+//     const { id } = req.params;
+//     const car = await Veehicle.findByIdAndUpdate(id, { ...req.body.list });
+//     res.redirect(`/lists/${list._id}`)
+// });
+
+// app.delete('/lists/:id', async (req, res) => {
+//     const { id } = req.params;
+//     await Vehicle.findByIdAndDelete(id);
+//     res.redirect('/lists');
+// })
+///////////////////////////////////////////////////////
+
 app.get('/login', (req, res) =>{
     // res.send("Login")
     res.render('Info/login')
